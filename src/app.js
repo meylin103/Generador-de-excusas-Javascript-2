a6248ac8abefcc31671adc6a849ff24a200035ed
meylin103 <92810092+meylin103@users.noreply.github.com>
2026-03-05 04:26:24 +0000

    Generador de excusa terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let whoExcusa = Math.floor(Math.random() * who.length);
+  let actionExcusa = Math.floor(Math.random() * action.length);
+  let whatExcusa = Math.floor(Math.random() * what.length);
+  let whenExcusa = Math.floor(Math.random() * when.length);
+
+  let excuse = who[whoExcusa] + " " + action[actionExcusa] + " " + what[whatExcusa] + " " + when[whenExcusa];
+
+document.getElementById("excuse").innerHTML = excuse;
+
 };
